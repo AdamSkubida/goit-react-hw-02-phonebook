d4bd6fa5073c023c619c8707d518dbd6e48d4fd7
AdamSkubida <adasiow.s@gmail.com>
2023-07-17 00:29:47 +0200

    feature/addContact function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    phone: '',
+  };
+
+  addContact = (newName, newPhone) => {
+    const exist = this.state.contacts.find(contact => contact.name === newName);
+    if (!exist) {
+      this.setState(prevState => ({
+        contacts: [
+          ...prevState.contacts,
+          {
+            name: newName,
+            id: nanoid(),
+            phone: newPhone,
+          },
+        ],
+      }));
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm />
+
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export class ContactList extends Component {
+  render() {
+    return <div>ContactList</div>;
+  }
+}
