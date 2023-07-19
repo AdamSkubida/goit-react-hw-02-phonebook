64e0c79d2fec9f01380fd1261777ac332bf231c4
AdamSkubida <adasiow.s@gmail.com>
2023-07-19 11:40:55 +0200

    ContactForm inputs pattern added

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -39,6 +39,7 @@ export class ContactForm extends Component {
             className={css.input}
             type="text"
             name="name"
+            pattern="^[A-Za-z.'\- ]+$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             onChange={this.handleNameChange}
@@ -52,6 +53,7 @@ export class ContactForm extends Component {
             className={css.input}
             type="tel"
             name="number"
+            pattern="^\+?\d{1,4}?\s?\(?\d{1,4}?\)?\s?\d{1,4}\s?\d{1,4}\s?\d{1,9}$"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             onChange={this.handlePhoneChange}
